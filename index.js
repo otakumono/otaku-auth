0d2ced0482eeb9801b8f4aa9a6b5e21219188346
Robert Lodico <rjlodico@ncsu.edu>
2015-04-03 12:49:45 -0400

    Node package bundling

---
+++ index.js
@@ -0,0 +1,9 @@
+var xport = require('node-xport')(module),
+	OtakuAuth = require('./lib/'),
+	OtakuCore = OtakuAuth.OtakuCore;
+
+OtakuAuth.prepare();
+OtakuAuth.run();
+
+/* Export the module */
+xport(OtakuAuth);
